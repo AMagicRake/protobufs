4ba034f3d4a41c78b98ca879a027ec9a29ad1f3a
AMagicRake <niel.gow@live.co.uk>
2024-07-21 09:30:19 +0100

    using a proto in go

--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package main
 
+import (
+	"fmt"
+	pb "proto_demo/proto"
+)
+
+func do_simple() *pb.Simple {
+	return &pb.Simple{
+		Id:         42,
+		IsSimple:   true,
+		Name:       "Niel",
+		SampleList: []int32{1, 2, 3, 4, 5, 6},
+	}
+}
+
 func main() {
-	return
+	fmt.Println(do_simple())
 }
